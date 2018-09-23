b732637145226e8f042beecc6b76aaa00be2a7e7
Stefanus17 <43497735+Stefanus17@users.noreply.github.com>
2018-09-23 18:53:20 +0700

    Add files via upload

---
+++ PBO3_10117109_Latihan1_Biodata1.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3_10117109_latihan1_biodata1;
+
+/**
+ *
+ * @author 
+ * NAMA  : Stefanus Saputra
+ * KELAS : IF 3
+ * NIM   : 10117109
+ */
+public class PBO3_10117109_Latihan1_Biodata1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("BIODATA ROKCSTAR" + "\n" + "--------");
+        System.out.println("NAMA\t\t: Stefanus Saputra");
+        System.out.println("ALAMAT\t\t: Jalan Tubagus Ismail VI no. 33");
+        System.out.println("TEMPAT LAHIR\t: Cirebon");
+        System.out.println("TANGGAL LAHIR\t: 17 Mei 1999");
+        System.out.println("-----------------------");
+        System.out.println("HOBI\t\t: Olahraga");
+        System.out.println("MAKES\t\t: Nasi Goreng");
+        System.out.println("MIKES\t\t: Air Putih");
+    }
+    
+}
